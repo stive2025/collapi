0d9c8357fbfac63fecdeb2f02256eff46aace633
cesen7432-bit <cesen7432@gmail.com>
2025-12-20 19:01:19 -0500

    feature: <campains> se agrega array de agentes con id y username

--- app/Models/Campain.php
+++ app/Models/Campain.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Campain extends Model
 {
@@ -20,4 +21,28 @@ class Campain extends Model
         'business_id'
     ];
 
+    protected $casts = [
+        'agents' => 'array',
+    ];
+
+    protected $appends = ['agents_details'];
+
+    /**
+     * Get the agents details with username and id
+     */
+    protected function agentsDetails(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                if (!$this->agents || !is_array($this->agents)) {
+                    return [];
+                }
+
+                return User::whereIn('id', $this->agents)
+                    ->select('id', 'username')
+                    ->get()
+                    ->toArray();
+            }
+        );
+    }
 }
